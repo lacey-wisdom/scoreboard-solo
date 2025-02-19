01b5b00884add26566d225a400c7536d7c315516
lacey-wisdom <lacey.wisdom@gmail.com>
2025-02-19 17:08:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,15 @@ let homeCounter = document.getElementById("home-counter")
 
 let homePts = 0 
 
-function add1PtHome() {
-    homePts += 1
-    homeCounter.textContent = homePts
-}
-
-function add2PtsHome() {
-    homePts += 2
-    homeCounter.textContent = homePts
-}
-
-function add3PtsHome() {
-    homePts += 3
+function addPointsHome(num) {
+    homePts += num
     homeCounter.textContent = homePts
 }
 
 let guestCounter = document.getElementById("guest-counter")
 let guestPts = 0 
 
-function add1PtGuest() {
-    guestPts += 1
+function addPointsGuest(num) {
+    guestPts += num
     guestCounter.textContent = guestPts
 }
-
-function add2PtsGuest() {
-    guestPts += 2
-    guestCounter.textContent = guestPts
-}
-
-function add3PtsGuest() {
-    guestPts += 3
-    guestCounter.textContent = guestPts
-}
-
-
-
-
